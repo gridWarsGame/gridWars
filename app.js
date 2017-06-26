6e3564bbdabdf013215e10d1e297242c3b72f15d
la januari <lajanuari@gmail.com>
2017-06-26 16:37:04 -0700

    created GameBoard and Coord object

---
+++ app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function GameBoard() {
+  this.grid = [];
+}
+
+GameBoard.prototype.updateBoard = function () {
+
+};
+GameBoard.prototype.setupBoard = function () {
+
+};
+
+function Coord () {
+  //the default is unseen; once coordinate is picked, status ==== hit || miss.
+  this.status = 'unseen';
+  this.sub = false;
+}
+
+Coord.prototype.guessed = function () {
+  if (this.sub){
+    this.status = 'hit';
+  }
+  else{
+    this.status = 'miss';
+  }
+};
