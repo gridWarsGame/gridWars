7e9c6ef3d172c292a68e43bac70b09686c0ad4dd
la januari <lajanuari@gmail.com>
2017-06-27 11:17:23 -0700

    added addSub function with x,y grid coordinates

--- app.js
+++ app.js
@@ -22,13 +22,19 @@ GameBoard.prototype.createGrid = function (size) {
   }
 };
 
+GameBoard.prototype.addSub = function (x,y) {
+  //subtract one so that grid coordinates start at 1.
+  this.grid[x - 1][y - 1].sub = true;
+};
+
 function Coord () {
   //the default is unseen; once coordinate is picked, status ==== hit || miss.
   this.status = 'unseen';
+  //this tells whether there is a sub at this location.
   this.sub = false;
 }
 
-Coord.prototype.guess = function () {
+Coord.prototype.guessed = function () {
   if (this.sub){
     this.status = 'hit';
     return true;
