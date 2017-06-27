7cdb96a26d5d2251f3d54b5a654c98a27a024206
dmuldrew <dmuldrew@gmail.com>
2017-06-26 17:49:34 -0700

    created array of reference to update table

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 'use strict';
-function makeGridTable() {
+function makeGridTable(size) {
+  var refSquares = [];
+
   var domTarget = document.getElementById('grid_table');
   var table = document.createElement('table');
-  for (var i = 0; i < array.length; i++) {
+
+  for (var i = 0; i < size; i++) {
     var row = document.createElement('tr');
-    for (var j = 0; j < array.length; j++) {
+    var refRow = [];
+
+    for (var j = 0; j < size; j++) {
       var square = document.createElement('td');
+      square.textContent = '-';
+      refRow.push(square);
+      row.appendChild(square);
     }
+
+    refSquares.push(refRow);
+    table.appendChild(row);
   }
+
+  domTarget.appendChild(table);
+
+  return refSquares;
 }
