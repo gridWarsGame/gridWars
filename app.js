2f90111bd1d5864fb5b4dc98d823fd81dc22199a
la januari <lajanuari@gmail.com>
2017-06-26 17:31:16 -0700

    created html for table, began dynamically creating table

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+function makeGridTable() {
+  var domTarget = document.getElementById('grid_table');
+  var table = document.createElement('table');
+  for (var i = 0; i < array.length; i++) {
+    var row = document.createElement('tr');
+    for (var j = 0; j < array.length; j++) {
+      var square = document.createElement('td');
+    }
+  }
+}
